9d8807d6fa4283b0c0398ee4e478eb0549b7c67d
Milan <milanmilan.mm55@gmail.com>
2026-06-18 20:10:33 +0200

    added hymission plugin

--- conf/keyboard.lua
+++ conf/keyboard.lua
@@ -122,3 +122,12 @@ hl.bind(mainMod .. " + SHIFT + W", function()
     monitor = "+1",
   }))
 end)
+
+---------------
+--- Plugins ---
+---------------
+
+hl.bind(mainMod .. " + TAB", hl.plugin.hymission.toggle)
+hl.bind(mainMod .. " + CTRL + TAB", function()
+  hl.plugin.hymission.open("onlycurrentworkspace")
+end)
