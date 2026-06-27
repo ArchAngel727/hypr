aae6af069128115f6f269647bb2ab239e4812d9b
Milan <milanmilan.mm55@gmail.com>
2026-06-27 23:06:55 +0200

    added plugin reload and gaps fix

--- conf/autostart.lua
+++ conf/autostart.lua
@@ -1,4 +1,5 @@
 hl.on("hyprland.start", function()
+  hl.exec_cmd("hyprpm reload")
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
   hl.exec_cmd("systemctl --user start opentabletdriver.service")
 
